9ac531e330230d4ce082d5ef31b6c1a9e215ce4d
viraj458 <virajmalaka94@gmail.com>
2024-04-24 21:20:11 +0530

    feat(experiment): 3D card and Hero Parralax

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import { BackgroundGradientDemo } from '@/app/BackgroundGradientDemo';
+import { ThreeDCardDemo } from '@/app/ThreeDCard';
+import { HeroParallax } from '@/components/ui/hero-parallax';
+import { products } from '@/utils/products';
+
 export default function Home() {
-  return <main>Hello</main>;
+  return (
+    <main className="bg-black">
+      <HeroParallax products={products} />
+      <div className="max-container padding-container">
+        <div className="flex justify-between flex-wrap">
+          <ThreeDCardDemo buttonText2="SignUp" />
+          <BackgroundGradientDemo />
+          <ThreeDCardDemo buttonText2="SayHello" />
+        </div>
+      </div>
+    </main>
+  );
 }
